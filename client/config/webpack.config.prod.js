f1dc4cdcd1e309a624287b26901a9ad696338038
hayden <hayden321@gmail.com>
2017-06-02 01:34:55 -0700

    added scss to prod config

--- client/config/webpack.config.prod.js
+++ client/config/webpack.config.prod.js
@@ -113,7 +113,9 @@ module.exports = {
           /\.(js|jsx)$/,
           /\.css$/,
           /\.json$/,
-          /\.svg$/
+          /\.svg$/,
+          /\.sass$/,
+          /\.scss$/,  
         ],
         loader: 'url',
         query: {
@@ -144,6 +146,11 @@ module.exports = {
         test: /\.css$/,
         loader: ExtractTextPlugin.extract('style', 'css?importLoaders=1!postcss')
         // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
+      },
+      {
+        test: /\.scss$/,
+        include: paths.appSrc,
+        loaders: ["style", "css", "sass"]
       },
       // JSON is not enabled by default in Webpack but both Node and Browserify
       // allow it implicitly so we also enable it.
